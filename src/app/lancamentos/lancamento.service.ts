4324cbb35a40b2ae0a0f470cedd240035b4c8d0a
Rogerio Aguiar <rfaguiar1@gmail.com>
2017-08-19 09:42:14 -0300

    17.8. Excluindo lançamentos e o decorador @ViewChild

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -53,4 +53,13 @@ export class LancamentoService {
         return resultado;
       });
   }
+
+  excluir(codigo: number): Promise<void> {
+    const headers = new Headers;
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.delete(`${this.lancamentosUrl}/${codigo}`, {headers})
+      .toPromise()
+      .then(() => null);
+  }
 }
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,5 @@
 import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import {LazyLoadEvent} from 'primeng/components/common/api';
 
@@ -13,6 +13,7 @@ export class LancamentosPesquisaComponent implements OnInit {
   totalRegistros = 0;
   filtro = new LancamentoFiltro();
   lancamentos = [];
+  @ViewChild('tabela') grid;
 
   constructor(private lancamentoService: LancamentoService){ }
 
@@ -30,6 +31,13 @@ export class LancamentosPesquisaComponent implements OnInit {
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.first = 0;
+      });
   }
 
 }
