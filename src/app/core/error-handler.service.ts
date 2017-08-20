799b8ff01963e3b6067ceda983eea11314909a31
Rogerio Aguiar <rfaguiar1@gmail.com>
2017-08-20 15:55:43 -0300

    19.14. E se o Refresh Token expirar?

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,16 +1,25 @@
+import { Router } from '@angular/router';
 import { ToastyService } from 'ng2-toasty';
 import { Injectable } from '@angular/core';
+
+import { NotAuthenticatedError } from './../seguranca/money-http';
 
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor(private toasty: ToastyService) { }
+  constructor(
+    private toasty: ToastyService,
+    private router: Router
+  ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
     } else if (errorResponse instanceof Response
         && errorResponse.status >= 400 && errorResponse.status <= 499) {
       let errors;
--- src/app/seguranca/money-http.ts
+++ src/app/seguranca/money-http.ts
@@ -6,6 +6,8 @@ import { Observable } from 'rxjs/Observable';
 
 import { AuthService } from './auth.service';
 
+export class NotAuthenticatedError {}
+
 @Injectable()
 export class MoneyHttp extends AuthHttp {
 
@@ -51,6 +53,10 @@ export class MoneyHttp extends AuthHttp {
 
       const chamadaNovoAccessToken = this.auth.obterNovoAccessToken()
         .then(() => {
+          if (this.auth.isAccessTokenInvalido()) {
+            throw new NotAuthenticatedError();
+          }
+
           return fn().toPromise();
         });
 
