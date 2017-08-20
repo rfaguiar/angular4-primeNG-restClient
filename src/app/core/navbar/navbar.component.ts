d870bcf22b9990130702c2159bb26445d4d49585
Rogerio Aguiar <rfaguiar1@gmail.com>
2017-08-20 14:33:09 -0300

    19.8. Exibindo o nome do usuário logado

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../seguranca/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor( private auth: AuthService) { }
 }
